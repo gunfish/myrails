936382772ba26d466f9cd8e45a710ea577192a81
JB <info@airsoft-base.de>
2010-11-21 23:26:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrails::Application.config.secret_token = 'd5a7c4b517192b848a68dd1f6cb2cf7a740a46d3114388d8b0a5ea79ff905a3f28dfa7c9916687f7293c548240d07005db21cfde84c08970936c96bfd9c727b5'
